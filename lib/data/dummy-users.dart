e0cf1ae7f4f2fec9df663c01e1a45bfc91181731
thiiagofernando <thiago.fernando@MSN.COM>
2020-08-05 16:48:22 -0400

    criado projeto

---
+++ lib/data/dummy-users.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://media.istockphoto.com/vectors/female-avatar-profile-icon-round-african-american-woman-face-vector-id897375110?k=6&m=897375110&s=170667a&w=0&h=OF0IH7R4EZ276ggHzgXXtSm58rDgxy8W7OJXiYC-31M='),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://www.shareicon.net/data/512x512/2016/05/24/770117_people_512x512.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBhtSPnmjr09ERciIU5qjGNMvcXnlNOcJOYA&usqp=CAU'),
+  '4': const User(
+      id: '4', name: 'Joao', email: 'joao@alunos.com.br', avatarUrl: null),
+};
